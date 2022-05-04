ed24c7fddea35acb4e7d2cd7372b9481d8fecad9
Eslam Waheed <e.waheed@bestbuy.local>
2022-05-04 12:08:45 +0200

    test expose

--- app/src/main/java/com/eslamwaheed/mvvmdemo/data/models/entities/Photos.kt
+++ app/src/main/java/com/eslamwaheed/mvvmdemo/data/models/entities/Photos.kt
@@ -4,6 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 @Entity
@@ -11,6 +12,7 @@ data class Photos(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "result_id")
     val result_id: Int,
     @SerializedName("width")
+    @Expose(serialize = true, deserialize = true)
     val width: Int?,
     @SerializedName("height")
     val height: Int?,
--- app/src/main/java/com/eslamwaheed/mvvmdemo/ui/MainActivity.kt
+++ app/src/main/java/com/eslamwaheed/mvvmdemo/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.eslamwaheed.mvvmdemo.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.eslamwaheed.mvvmdemo.data.models.entities.Photos
 import com.eslamwaheed.mvvmdemo.databinding.ActivityMainBinding
+import com.google.gson.GsonBuilder
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val gson = GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+        val photo = Photos(
+            result_id = 0,
+            width = 1,
+            height = 2,
+            description = "123",
+            altDescription = "567",
+            urls = null
+        )
+        Log.e("main", gson.toJson(photo))
     }
 }
